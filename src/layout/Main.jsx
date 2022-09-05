e3a87d04f82f5c780c6deddb75606d5b158a7855
nikolay <per-mail@mail.ru>
2022-09-05 16:24:18 +0700

    05.09

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -43,7 +43,7 @@ class Main extends React.Component {
             // /добавляем исключения
             .catch((err) => {
                 console.error(err);
-                this.setState({ loading: false })
+                this.setState({ loading: false });
             });
     };
 
